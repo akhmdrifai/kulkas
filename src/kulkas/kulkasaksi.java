0d3cfbd6b21382e2c7d1c1eb69a9dbac161ee2ab
akhmdrifai <32448266+akhmdrifai@users.noreply.github.com>
2018-03-26 22:25:10 +0800

    Membuat Class Aksi dan Method main

---
+++ src/kulkas/kulkasaksi.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kulkas;
+
+/**
+ *
+ * @author user
+ */
+public class kulkasaksi {
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
