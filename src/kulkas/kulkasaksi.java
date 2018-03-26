92664fe9057c393937ee0f08e0ea00f8881f3045
akhmdrifai <32448266+akhmdrifai@users.noreply.github.com>
2018-03-26 22:42:27 +0800

    Mengisi Atribut

--- src/kulkas/kulkasaksi.java
+++ src/kulkas/kulkasaksi.java
@@ -13,6 +13,30 @@ public class kulkasaksi {
     public static void main(String[] args) {
         
         Classkulkas adrian = new Classkulkas();
+        System.out.println("kulkas adrian");
+        adrian.merek="samsung";
+        adrian.harga=8000000;
+        adrian.jenis="3 pintu";
+        adrian.warna="putih tosca";
+        adrian.pendingin();
+        
+        Classkulkas aska = new Classkulkas();
+        System.out.println("kulkas aska");
+        aska.merek="LG";
+        aska.harga=5000000;
+        aska.jenis="2 pintu";
+        aska.warna="biru muda";
+        aska.pendingin();
+        
+        Classkulkas fadlan = new Classkulkas();
+        System.out.println("kulkas fadlan");
+        fadlan.merek="toshiba";
+        fadlan.harga=7000000;
+        fadlan.jenis="3 pintu";
+        fadlan.warna="hijau tosca";
+        fadlan.pendingin();
+        
+        
         
     }
 }
